ad09cd88543372136d206536c419057b5e82142f
kensinaon <kensinaon@gmail.com>
2022-01-06 20:50:32 +0800

    add

---
+++ src/main/java/com/sinaon/fleetmsv23/ApplicationController.java
@@ -0,0 +1,48 @@
+package com.sinaon.fleetmsv2;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+/**
+ * @author ksinaon
+ * @since Dec 16, 2021
+ */
+
+@Controller
+public class ApplicationController {
+
+	@GetMapping("/index")
+	public String home() {
+		return "index";
+	}
+	
+	@GetMapping("/hr")
+	public String hr() {
+		return "/hr/index";
+	}
+	
+	@GetMapping("/fleet")
+	public String fleet() {
+		return "/fleet/index";
+	}
+	
+	@GetMapping("/helpdesk")
+	public String helpdesk() {
+		return "/helpdesk/index";
+	}
+	
+	@GetMapping("/accounts")
+	public String accounts() {
+		return "/accounts/index";
+	}
+	
+	@GetMapping("/payroll")
+	public String payroll() {
+		return "/payroll/index";
+	}
+	
+	@GetMapping("/parameters")
+	public String parameters() {
+		return "/parameters/index";
+	}
+}
