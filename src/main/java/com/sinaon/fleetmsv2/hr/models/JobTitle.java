48606637d127b316b929aef511bf84effc52915d
kensinaon <kensinaon@gmail.com>
2022-01-07 14:13:34 +0800

    Added HR models

---
+++ src/main/java/com/sinaon/fleetmsv2/hr/models/JobTitle.java
@@ -0,0 +1,18 @@
+package com.sinaon.fleetmsv2.hr.models;
+
+import javax.persistence.Entity;
+
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
+import com.sinaon.fleetmsv2.parameters.models.CommonObject;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@Entity
+@Data
+@EqualsAndHashCode(callSuper=false)
+@JsonIdentityInfo(generator = ObjectIdGenerators.PropertyGenerator.class, property = "id")
+public class JobTitle extends CommonObject {
+
+}
