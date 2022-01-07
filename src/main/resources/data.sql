e1428ef4cf2c63bcdd80dd10786e66365a892e03
kensinaon <kensinaon@gmail.com>
2022-01-07 18:39:05 +0800

    Added sampleData-sql

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+insert into Country value(1, 'Washington DC', 'USA', 'North America', 'United States', 'American');
+insert into Country value(2, 'New Delhi', 'IN', 'India', 'India', 'Indian');
+insert into Country value(3, 'Budapest', 'HUN', 'India', 'India', 'Indian');
